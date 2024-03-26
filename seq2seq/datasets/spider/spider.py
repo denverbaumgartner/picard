87eba1ea3c637d7c5ad860012a799237ab85865f
denver <denver@semiotic.ai>
2024-03-26 14:56:06 +0000

    datasets/spider: resolve google drive virus check

--- seq2seq/datasets/spider/spider.py
+++ seq2seq/datasets/spider/spider.py
@@ -42,7 +42,7 @@ _HOMEPAGE = "https://yale-lily.github.io/spider"
 
 _LICENSE = "CC BY-SA 4.0"
 
-_URL = "https://drive.google.com/uc?export=download&id=1_AckYkinAnhqmRQtGsQgUKAnTHxxX5J0"
+_URL = "https://drive.google.com/uc?export=download&id=1_AckYkinAnhqmRQtGsQgUKAnTHxxX5J0&confirm=t"
 
 
 class Spider(datasets.GeneratorBasedBuilder):
